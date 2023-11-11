47727c5e3330abaf3866d6fa7e5f1d57fb7b6eda
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2023-11-11 14:29:07 +0100

    Add check if group exist and add function to create subgroup test

--- GitLabAutomater.py
+++ GitLabAutomater.py
@@ -19,6 +19,26 @@ data = {
     "name": "PythonAdvancedELIVERMEULEN",
     "path": "PythonAdvancedELIVERMEULEN-group"
 }
-postrequest = requests.post(api_groups_url,headers=headers, data=data)
 
+get_existing_group_url = f"https://gitlab.apstudent.be/api/v4/groups?search={data['name']}"
+get_existing_group_request = requests.get(get_existing_group_url, headers=headers)
+
+existing_group = get_existing_group_request.json()
+
+#check if the group already exist or not and stores the id of set group
+if existing_group:
+    group_id = existing_group[0].get("id")
+else:
+    postrequest = requests.post(api_groups_url,headers=headers, data=data)
+    print(postrequest.text)
+    group_id = postrequest.json().get("id")
+
+#creates a subgroup test in the main group created above
+data= {
+    "name": "test",
+    "path": "test-subgroup",
+    "parent_id": group_id
+}
+
+postrequest = requests.post(api_groups_url,headers=headers,data=data)
 print(postrequest.text)
