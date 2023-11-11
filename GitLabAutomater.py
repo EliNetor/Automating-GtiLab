3dee02f8b397c01f6136d8bb1c7ae31420aecfe0
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2023-11-11 14:05:03 +0100

    Add feature to create a group

--- GitLabAutomater.py
+++ GitLabAutomater.py
@@ -3,12 +3,22 @@ import getpass
 
 #Tests connection and gives information about the user of the token
 token = getpass.getpass("Token: ")
-api_url = "https://gitlab.apstudent.be/api/v4/user"
+api_users_url = "https://gitlab.apstudent.be/api/v4/user"
 
 headers = {
     "Authorization": f"Bearer {token}"
 }
 
-response = requests.get(api_url, headers=headers)
+response = requests.get(api_users_url, headers=headers)
 
 print(response.json())
+
+#Creates a group PythonAdvancedELIVERMEULEN
+api_groups_url = "https://gitlab.apstudent.be/api/v4/groups"
+data = {
+    "name": "PythonAdvancedELIVERMEULEN",
+    "path": "PythonAdvancedELIVERMEULEN-group"
+}
+postrequest = requests.post(api_groups_url,headers=headers, data=data)
+
+print(postrequest.text)
