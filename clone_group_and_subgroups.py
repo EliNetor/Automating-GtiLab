2cba36cb51160a8b77dca324905fdd3755d4e8b4
Eli Vermeulen <eli.vermeulen@student.ap.be>
2023-11-15 12:47:09 +0100

    Add function to clone gitlab repos

--- clone_group_and_subgroups.py
+++ clone_group_and_subgroups.py
@@ -4,10 +4,12 @@ import getpass
 studenten_nummer = input("Geef je studenten nummer op: ")
 group_naam = input("Van welke groep wil je de projecten klonen? ")
 token = getpass.getpass("Token: ")
+local_path = "C:/Users/elive/OneDrive/Documenten/"
 
 headers = {
     "Authorization": f"Bearer {token}"
 }
 
 project_ids = f.ShowProjects(group_naam,headers)
-print(project_ids)
+
+f.CloneRepo(project_ids,headers,local_path)
--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import requests
+import git
 
 #returns a list of poject id's for given group name
 def ShowProjects(group_name,headers):
@@ -18,3 +19,20 @@ def ShowProjects(group_name,headers):
             project_ids.append(projects.get("id"))
 
     return project_ids
+
+#clones the repo from the given ids, the root dir is set to document and the cloned dir wil get the name of their id's
+def CloneRepo(ids,headers,local_directory):
+    og_dir = local_directory
+    for id in ids:
+        local_directory = og_dir
+        url = f"https://gitlab.apstudent.be/api/v4/projects/{id}"
+        req = requests.get(url,headers=headers)
+        
+        repo_info = req.json()
+        repo_http = repo_info["http_url_to_repo"]
+
+        local_directory += str(id)
+        try:
+            git.Repo.clone_from(repo_http, local_directory)
+        except git.exc.GitError:
+            pass
